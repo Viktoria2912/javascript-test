a6aa785d24cb0902964883d4737124c28cfd0373
Виктория <114413240+Viktoria2912@users.noreply.github.com>
2023-04-20 21:28:18 +0300

    1

---
+++ js.js
@@ -0,0 +1,182 @@
+"use strict";
+
+// console.log("artem".toUpperCase());
+// console.log("vika".toUpperCase());
+// console.log("js".toLocaleUpperCase());
+// console.log("aaaaaaaaaaaaaaaaaaaaaa".toLowerCase());
+
+// const name = "vika";
+// const age = 35;
+// const country = "ukraine";
+
+// console.log(name);
+// console.log(age);
+// console.log(country);
+
+// age = 10;
+// age = 12;
+// console.log(age);
+
+//
+// let a;
+// console.log(typeof a);
+
+// const message = prompt(9);
+// console.log(typeof message);
+
+// const x = 100;
+// const y = 23;
+// console.log(x % y);
+
+// let x = 10;
+// x += 111;
+// console.log(x);
+
+// const x = 7;
+// const b = "hello";
+// console.log(typeof Number(x));
+// console.log(Number(b));
+
+// const valueB = "random string";
+// console.log(Number(valueB)); // NaN
+// console.log(typeof Number(valueB)); // "number"
+
+// console.log(Number.parseInt("7 px"));
+// console.log(Number.parseInt("12,444"));
+// console.log(Number.parseInt("13.44444"));
+// console.log(Number.parseInt("hello 14"));
+
+// console.log(Number.parseFloat("8 px"));
+// console.log(Number.parseFloat("8 ytlll px"));
+// console.log(Number.parseFloat("8,88888888 px"));
+// console.log(Number.parseFloat("9999.56567767676"));
+
+// console.log(Number.isNaN(8));
+// console.log(Number.isNaN("12"));
+// console.log(Number.isNaN("jkkkjkj"));
+// console.log(Number.isNaN(7.0909));
+// console.log(Number.isNaN(34, 9090));
+
+// const t = Number("67");
+// console.log(Number.isNaN(t));
+// const o = Number(58);
+// console.log(Number.isNaN(o));
+// const z = Number("fjhjkfjljlj");
+// console.log(Number.isNaN(z));
+// const d = Number(hkjkjjjhut);
+// console.log(Number.isNaN(d));
+// const s = Number("89.00000000");
+// console.log(Number.isNaN(s));
+// const u = Number("855,989898");
+// console.log(Number.isNaN(u));
+
+// console.log(0.1 + 0.3 === 0.4);
+// console.log(0.1 + 0.3);
+
+// console.log(0.1 + 0.2 === 0.3);
+// console.log(0.1 + 0.2);
+
+// console.log(0.17 + 0.28 === 0.45);
+// console.log(0.17 + 0.28);
+// console.log((0.17 * 100 + 0.28 * 100) / 100);
+// console.log((0.17 * 100 + 0.24 * 100) / 100);
+
+// console.log((0.56657585).toFixed(3));
+// // console.log("1.23344".toFixed(2));
+// console.log((0.123 + 0.456).toFixed(1));
+// console.log(0.123 + 0.456);
+// console.log((0.11 + 0.22).toFixed(1));
+
+// console.log(Math.floor(0.123));
+// console.log(Math.floor(2.134));
+
+// console.log(Math.ceil(1.7));
+// console.log(Math.ceil(1.2));
+// console.log(Math.ceil(2.1));
+
+// console.log(Math.round(356.909));
+// console.log(Math.round(1.3));
+// console.log(Math.round(3.1));
+
+// console.log(Math.round(1.2));
+// console.log(Math.round(1.5));
+// console.log(Math.round(2.4)); // 1
+// console.log(Math.round(2.9));
+
+// console.log(Math.max(0, 3445, 5, 27874, 788, 89));
+// console.log(Math.min(1, 778, 23, 56, 2, 6));
+
+// console.log(Math.pow(2, 2));
+// console.log(Math.pow(2, 3));
+// console.log(Math.pow(2, 6));
+// console.log(Math.pow(2, 10));
+
+// console.log(Math.random());
+// console.log(Math.random() * (10 - 1) + 1);
+
+//console.log(Math.random());
+// console.log(Math.random() * (6 - 1) + 1);
+
+// console.log(1 + 2 + "4"); // "34"
+
+// const a = 1 + 2 + "4";
+// console.log(typeof a);
+
+// const name = "artem";
+// console.log(name.length);
+
+// console.log("bjshuhduhuddef".length);
+
+// const a = `vika`;
+// console.log(a.length);
+
+// const s = "artem";
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+
+// const a = "ARTEM";
+// const b = "Artem";
+// // console.log(a === b);
+// console.log(a);
+// console.log(b);
+// console.log(a === b.toUpperCase());
+
+// const a =
+//   "hjfhgjjigiorioguioreuoguoiruuoruoiuguugiouiotugouoiuuuuuug  artem and vika";
+// console.log(a.indexOf("and"));
+// console.log(a.indexOf("the"));
+
+// const a = "artem";
+// console.log(a.includes("t"));
+// console.log(a.includes("artem"));
+// console.log(a.includes("ARTEM"));
+// console.log(a.includes("Artem"));
+
+// const b = "vika";
+// console.log(b.endsWith("a"));
+// console.log(b.endsWith("ika"));
+// console.log(b.endsWith("d"));
+// console.log(b.endsWith("vika"));
+// console.log(b.endsWith("s"));
+
+// const jsFileName = "script.js";
+// const minifiedJsFileName = jsFileName.replace(".js", ".min.js");
+// console.log(minifiedJsFileName); // "script.min.js"
+
+// const oldName = "script";
+// const newName = oldName.replace("script", "script.js");
+// console.log(newName);
+
+// const a = "gmail";
+// const b = a.replace("gmail", "gmail.com");
+// console.log(b);
+
+// const cssFileNames = "styles.css, about.css, portfolio.css";
+// const minifiedCssFileNames = cssFileNames.replaceAll(".css", ".min.css");
+// console.log(minifiedCssFileNames); // "styles.min.css, about.min.css, portfolio.min.css"
+
+// const a = "vika burlaka";
+// console.log(a.slice(0, 4));
+// console.log(a.slice(5, 12));
+// console.log(a.slice(0, a.length));
+// console.log();
